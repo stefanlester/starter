11c4c117499d65a4789b9230f25075a068cb6036
Stefan <stefanamoah001@gmail.com>
2021-11-11 11:05:00 +0100

    postman setup

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,25 @@
 const getAllTasks = (req, res) => {
-    res.send('all items from file')
+    res.send('get all tasks')
 }
 
+const createTask = (req, res) => {
+    res.json(req.body)
+}
+
+const getTask = (req, res) => {
+    res.send('get single task')
+}
+
+const updateTask = (req, res) => {
+    res.send('update task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('delete task')
+}
+
+
+
 module.exports = {
-    getAllTasks,
+    getAllTasks, createTask, getTask, updateTask, deleteTask
 }
